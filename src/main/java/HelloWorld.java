ec3c51f3829b68f5d5dda5be7fe85f8efc6ac65d
vmvn <wangchenmo1025@gmail,com>
2017-11-05 02:07:37 +0800

    2

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,12 +1,28 @@
+import com.testgit.kit.PropertiesUtil;
+
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class HelloWorld {
-    public static void main(String[] args) {
-        String s = Arrays.toString(new int[]{1, 2, 3});
-        System.out.println(s);
+    public static void main(String[] args) throws FileNotFoundException {
+//        String s = Arrays.toString(new int[]{1, 2, 3});
+//        System.out.println(s);
+//
+//        System.out.println("Six Six Six!My first open source github app!");
 
-        System.out.println("Six Six Six!My first open source github app!");
+       // String test = PropertiesUtil.getPro("", "test");
+       // System.out.println(test);
+        //PropertiesUtil.checkProprietsTime("test");
+
+        Scanner sc = new Scanner(System.in);
+        while (true){
+            String pname =  sc.next();
+            String test = new PropertiesUtil().getPro(pname, "test");
+            System.out.println(test);
+        }
+
     }
 
 
---
+++ src/main/java/com/testgit/kit/PropertiesUtil.java
@@ -0,0 +1,61 @@
+package com.testgit.kit;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Properties;
+
+/**
+ * @author wangjiepc
+ * @create 2017-11-05 1:35
+ * @desc 配置文件工具类
+ * @email wangchenmo1025@gmail.com
+ **/
+public class PropertiesUtil {
+
+    private static HashMap<String,Properties> propertiesHashMap = new HashMap<>();
+    private static HashMap<String,Long> propertiesTimeHashMap = new HashMap<>();
+
+    /**
+     *
+     */
+    public static void checkProprietsTime(String pname){
+        String path = PropertiesUtil.class.getResource("/prop/"+pname+".properties").getPath();
+        File file = new File(path);
+        long lastModified = file.lastModified();
+
+    }
+
+    /**
+     * 获取配置文件值
+     * @param pname
+     * @param pvalue
+     * @return
+     */
+    public String getPro(String pname,String pvalue) throws FileNotFoundException {
+        String path = PropertiesUtil.class.getResource("/prop/"+pname+".properties").getPath();
+        File file = new File(path);
+        long lastModified = file.lastModified();
+//        //检查缓存中是否存在
+//        if(propertiesHashMap.containsKey(pname)){
+//            //检查是否修改过
+//            if(null != propertiesTimeHashMap.get(pname)&&propertiesTimeHashMap.get(pname).equals(lastModified)){
+//                return (String)propertiesHashMap.get(pname).get(pvalue);
+//            }
+//        }
+
+        Properties pro = new Properties();
+        //InputStream resourceAsStream = PropertiesUtil.class.getResourceAsStream("/prop/"+pname+".properties");
+        FileInputStream resourceAsStream = new FileInputStream(file);
+        try {
+            pro.load(resourceAsStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //缓存
+        propertiesHashMap.put(pname,pro);
+        propertiesTimeHashMap.put(pname,lastModified);
+        return pro.getProperty(pvalue);
+    }
+
+
+}
